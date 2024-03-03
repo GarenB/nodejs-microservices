e70540560ecd2be5b126090845939613c77b7b2a
Garen <boygaren@gmail.com>
2024-03-03 12:37:14 +0200

    feature: created error handler interceptor and tryCatch function to catch errors

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const dotenv = require("dotenv").config({ silent: true });
 
 const accountRoute = require("./lib/routes/account");
 const defaultRouter = require("./lib/routers/default");
+const errorHandlerMiddleware = require("./lib/errors/errorHandlerMiddleware");
 
 // Create an Express application
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/account", accountRoute(defaultRouter()));
+
+errorHandlerMiddleware(app);
 
 // Start the server
 app.listen(port, () => {

fabec0d0e507814b2cdb99f8f6586e27b5ae986d
Garen <boygaren@gmail.com>
2024-03-05 21:44:26 +0200

    feature: added getAllUsers route

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config({ silent: true });
 
+// routes imports
 const accountRoute = require("./lib/routes/account");
+const usersRoute = require("./lib/routes/users");
+
 const defaultRouter = require("./lib/routers/default");
 const errorHandlerMiddleware = require("./lib/errors/errorHandlerMiddleware");
 
@@ -9,7 +12,9 @@ const errorHandlerMiddleware = require("./lib/errors/errorHandlerMiddleware");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// routes
 app.use("/account", accountRoute(defaultRouter()));
+app.use("/users", usersRoute(defaultRouter()));
 
 errorHandlerMiddleware(app);
 

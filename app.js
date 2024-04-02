aae3b86d349ea1bc44da67178fdf1cef8d4f17fe
Garen <boygaren@gmail.com>
2024-04-02 23:46:20 +0300

    chore: renamed defaultRouter to DefaultRouter

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv").config({ silent: true });
 const accountRoute = require("./lib/routes/account");
 const usersRoute = require("./lib/routes/users");
 
-const defaultRouter = require("./lib/routers/default");
+const DefaultRouter = require("./lib/routers/default");
 const AuthenticatedRouter = require("./lib/routers/authenticated");
 const errorHandlerMiddleware = require("./lib/errors/errorHandlerMiddleware");
 
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // routes
-app.use("/account", accountRoute(defaultRouter()));
+app.use("/account", accountRoute(DefaultRouter()));
 app.use("/users", usersRoute(AuthenticatedRouter()));
 
 errorHandlerMiddleware(app);
